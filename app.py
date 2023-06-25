42d10ac5a11f98f833bd4bcf7ed4690f09ae906b
pagonzal13 <paulagg176@gmail.com>
2023-06-25 13:33:08 +0200

    feat: sections style focus on UX

--- app.py
+++ app.py
@@ -6,6 +6,16 @@ import time, os, sys, traceback
 from threading import Thread
 from streamlit.runtime.scriptrunner import add_script_run_ctx
 
+st.set_page_config(
+    page_title="MLOptimizer",
+    page_icon="💻",
+    layout="wide"
+)
+
+st.header('MLOptimizer')
+st.subheader('Find the best hyper-parameters for training your data!')
+st.divider()
+
 target = ''
 algorithm = ''
 individuals = 10
@@ -62,7 +72,19 @@ def set_custom_params(fixed_rows, range_rows):
         custom_params_diccionary[param_name] = param
 
 def optimize(optimizer):
-    optimizer.optimize_clf(individuals, generations, checkpoint)
+    try:
+        optimizer.optimize_clf(individuals, generations, checkpoint)
+    except Exception as err:
+        st.error('Oops...Caparrini has to work more (but maybe you should check your input data, selected target, amount of individuals and generations...)', icon="🚨")
+        name = type(err).__name__
+        st.error(name + ': ' + str(err))
+    else:
+        st.success('Optimization has been successfully generated!', icon="✅")
+        set_session_state_vars(
+            last_population_path_param = os.path.join(optimizer.results_path, "populations.csv"),
+            last_logbook_path_param = os.path.join(optimizer.results_path, "logbook.csv"),
+            show_results_param = True
+        )
 
 def generations_status_bar():
     latest_generation = st.empty()
@@ -83,40 +105,29 @@ def individuals_status_bar():
         time.sleep(0.1)
 
 def execute():
-    optimizer = None
-    try:
-        optimizer = eval(algorithm+'(x, y, custom_params=custom_params_diccionary, custom_fixed_params=custom_fixed_params_diccionary)')
+    optimizer = eval(algorithm+'(x, y, custom_params=custom_params_diccionary, custom_fixed_params=custom_fixed_params_diccionary)')
 
-        thread_1 = Thread(target=optimize, args=[optimizer])
-        thread_2 = Thread(target=generations_status_bar)
-        thread_3 = Thread(target=individuals_status_bar)
+    thread_1 = Thread(target=optimize, args=[optimizer])
+    thread_2 = Thread(target=generations_status_bar)
+    thread_3 = Thread(target=individuals_status_bar)
 
-        add_script_run_ctx(thread_1)
-        add_script_run_ctx(thread_2)
-        add_script_run_ctx(thread_3)
+    add_script_run_ctx(thread_1)
+    add_script_run_ctx(thread_2)
+    add_script_run_ctx(thread_3)
 
-        threads = [thread_1, thread_2, thread_3]
+    threads = [thread_1, thread_2, thread_3]
 
-        for t in threads:
-            t.start()
-        
-        for t in threads:
-            t.join()
-        #TO DO: reflect csv generation progress
-        
-
-    except Exception as err:
-        st.error('Oops...Caparrini has to work more (but maybe you should check your input data, selected target, amount of individuals and generations...)', icon="🚨")
-        name = type(err).__name__
-        st.error(name + ': ' + str(err))
-
-    else:
-        st.success('Optimization has been successfully generated!', icon="✅")
-
+    for t in threads:
+        t.start()
+    
+    for t in threads:
+        t.join()
+    #TO DO: reflect csv generation progress
+    
     return optimizer
 
-def download_files(population_path, logbook_path):
-    if population_path is not '' and logbook_path is not '':
+def download_files(population_path='', logbook_path=''):
+    if population_path is not '':
         with open(population_path) as file:
             btn_p = st.download_button(
                     label="Download populations.csv",
@@ -124,6 +135,7 @@ def download_files(population_path, logbook_path):
                     file_name="populations.csv",
                     mime="text/csv"
                 )
+    if logbook_path is not '':
         with open(logbook_path) as file:
             btn_l = st.download_button(
                     label="Download logbook.csv",
@@ -152,91 +164,101 @@ def set_session_state_vars(last_population_path_param, last_logbook_path_param, 
     st.session_state.last_logbook_path = last_logbook_path_param
     st.session_state.show_results = show_results_param
     
-st.info('Pay attention to the quality of your input data (column names, types of values, consistency, etc).', icon="ℹ️")
-input_csv_file = st.file_uploader("Upload your input file")
+input_csv_file = st.file_uploader("Upload your input file", type='csv', help=':information_source: Pay attention to the quality of your input data (column names, types of values, consistency, etc).')
 
 if input_csv_file is not None:
-    df = pd.read_csv(input_csv_file)
-    st.data_editor(df, use_container_width=True)
     
-    target = st.selectbox(
+    df = pd.read_csv(input_csv_file)
+    with st.expander("Review and/or edit your data"):
+        st.data_editor(df, use_container_width=True)
+
+    st.divider()
+
+    target, algorithm, genetic_params = st.tabs(["Target", "Algorithm", "Genetic params"])
+
+    with target:
+        target = st.selectbox(
         'Which column do you want to use as target?',
         df.columns)
 
-    y = df[target]
-    x = df.drop(target, axis=1)
+        y = df[target]
+        x = df.drop(target, axis=1)
 
-    algorithm = st.radio(
-        "Which algorithm would you like to use?",
-        ('TreeOptimizer',
-            'ForestOptimizer',
-            'ExtraTreesOptimizer',
-            'GradientBoostingOptimizer',
-            'XGBClassifierOptimizer',
-            'CatBoostClassifierOptimizer',
-            'KerasClassifierOptimizer',
-            'SVCOptimizer'))
-    
-    use_custom_params = st.checkbox('Use custom params')
-
-    if use_custom_params:
-        st.write("Edit the table below with the hyper-params values you want")
-        st.info("By default, parameters use ranges (they are not fixed). You can mark 'fixed' column of the parameters you want to set with a fixed value and set it in corresponding column.", icon="🤓")
-
-        edited_df = st.data_editor(
-            get_dataframe(algorithm),
-            hide_index=True,
-            use_container_width=True,
-            column_config={
-                "fixed value": st.column_config.NumberColumn(),
-                "range min": st.column_config.NumberColumn(),
-                "range max": st.column_config.NumberColumn(),
-                "denominator": st.column_config.NumberColumn()
-            },
-            disabled=("hyper-param", "type")
-        )
+    with algorithm:
+        algorithm = st.radio(
+            "Which algorithm would you like to use?",
+            ('TreeOptimizer',
+                'ForestOptimizer',
+                'ExtraTreesOptimizer',
+                'GradientBoostingOptimizer',
+                'XGBClassifierOptimizer',
+                'CatBoostClassifierOptimizer',
+                'KerasClassifierOptimizer',
+                'SVCOptimizer'))
         
-        fixed_rows = edited_df.loc[edited_df["use fixed"] == True]
-        range_rows = edited_df.loc[edited_df["use fixed"] == False]
+        use_custom_params = st.checkbox('Use custom params')
 
-        set_custom_params(fixed_rows, range_rows)
+        if use_custom_params:
+            st.write("Edit the table below with the hyper-params values you want")
+            st.info("By default, parameters use ranges (they are not fixed). You can mark 'fixed' column of the parameters you want to set with a fixed value and set it in corresponding column.", icon="🤓")
 
-    else:
-        custom_params_diccionary = {}
-        custom_fixed_params_diccionary = {}
-        
-    individuals = st.select_slider(
-        'Select the amount of individuals',
-        range(2, 101),
-        value = individuals)
-    generations = st.select_slider(
-        'Select the amount of generations',
-        range(2, 101),
-        value = generations)
+            edited_df = st.data_editor(
+                get_dataframe(algorithm),
+                hide_index=True,
+                use_container_width=True,
+                column_config={
+                    "fixed value": st.column_config.NumberColumn(),
+                    "range min": st.column_config.NumberColumn(),
+                    "range max": st.column_config.NumberColumn(),
+                    "denominator": st.column_config.NumberColumn()
+                },
+                disabled=("hyper-param", "type")
+            )
+            
+            fixed_rows = edited_df.loc[edited_df["use fixed"] == True]
+            range_rows = edited_df.loc[edited_df["use fixed"] == False]
+
+            set_custom_params(fixed_rows, range_rows)
+
+        else:
+            custom_params_diccionary = {}
+            custom_fixed_params_diccionary = {}
+
+    with genetic_params:
+        individuals = st.select_slider(
+            'Select the amount of individuals',
+            range(2, 101),
+            value = individuals)
+        generations = st.select_slider(
+            'Select the amount of generations',
+            range(2, 101),
+            value = generations)
     
     inizialize_session_state_vars()
+
+    st.divider()
 
     if st.button('Start new execution'):
         restart_session_state_vars()
 
         optimizer = execute()
 
-        set_session_state_vars(
-            last_population_path_param = os.path.join(optimizer.results_path, "populations.csv"),
-            last_logbook_path_param = os.path.join(optimizer.results_path, "logbook.csv"),
-            show_results_param = True
-        )
-
     if st.session_state.show_results is not False:
-        with open(st.session_state.last_population_path) as file:
-            df_output = pd.read_csv(file)
-            st.table(df_output)
-            file.seek(0)
+        population, logbook = st.tabs(["Population", "LogBook"])
 
-        with open(st.session_state.last_logbook_path) as file:
-            df_output = pd.read_csv(file, usecols=['avg','min','max'])
-            #need to be rescaled by using altair charts
-            st.line_chart(df_output)
-    
-    download_files(st.session_state.last_population_path, st.session_state.last_logbook_path)
+        with population:
+            with open(st.session_state.last_population_path) as file:
+                download_files(population_path=st.session_state.last_population_path)
+
+                df_output = pd.read_csv(file)
+                st.table(df_output)
+                file.seek(0)
+                
+        with logbook:
+            with open(st.session_state.last_logbook_path) as file:
+                download_files(logbook_path=st.session_state.last_logbook_path)
+
+                df_output = pd.read_csv(file, usecols=['avg','min','max'])
+                #need to be rescaled by using altair charts
+                st.line_chart(df_output)
     
